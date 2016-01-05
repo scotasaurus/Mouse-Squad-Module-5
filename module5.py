8d701707da0b01834f1d7ee13ca27a560c1718ae
Scot McIntosh <scot@outlook.com>
2016-01-04 22:30:48 -0800

    Update to module 5
    Routing url to sample HTML.

--- module5.py
+++ module5.py
@@ -1,7 +1,7 @@
 import urllib
 import urllib2
 
-urlToRequest = "http://www.google.com/"
+urlToRequest = "https://raw.githubusercontent.com/scotasaurus/Mouse-Squad-Module-5/master/sample.html"
 
 request = urllib2.Request(urlToRequest)
 response = urllib2.urlopen(request)
